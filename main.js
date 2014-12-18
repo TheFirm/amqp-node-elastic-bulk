a30b9cb17f8bde00028ba7de0af4d1232c0e8566
nickcuper <55dmitrii@gmail.com>
2014-12-18 20:56:01 +0200

    add IO

--- main.js
+++ main.js
@@ -10,22 +10,18 @@ var elastic = new require('./elastic');
 var intel = new require('./logger');
 var sendGrid = new require('./sendGrid');
 
-var io = require('socket.io')(3001);
+var io = require('socket.io').listen(3001);
 
-io.on('connection', function (socket) {
-    io.emit('typing', { will: 'be received by everyone'});
+
+io.sockets.on('connection', function (socket) {
+    io.sockets.emit('typing');
     intel.getLogger('app').info('IO connection');
-    socket.on('typing', function (from, msg) {
-        intel.getLogger('app').info('IO typing');
-        console.log('I received a private message by ', from, ' saying ', msg);
-    });
 
     socket.on('disconnect', function () {
-        intel.getLogger('app').info('IO disconnect');
-        io.sockets.emit('user disconnected');
+        intel.getLogger('app').info('IO disconect');
+
     });
 });
-
 var countMessagesPerBulk = 0;
 var messageBuffer = [];
 
